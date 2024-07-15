474530f6647a11a6949fb20d009514ff8f4defd7
SV-sites <sonkauram@gmail.com>
2024-07-15 11:19:39 +0200

    practice

---
+++ Practice1/practice_1.sql
@@ -0,0 +1,24 @@
+
+DROP TABLE Experiment;
+
+CREATE TABLE Experiment (
+	Id INTEGER NOT NULL PRIMARY KEY,
+	Name TEXT,
+	Result	INT,
+	Weight REAL
+);
+
+INSERT INTO Experiment (Name, Result, Weight) VALUES ('Dario', 31, 30.12);
+INSERT INTO Experiment (Name, Result, Weight) VALUES ('Maria', 33, 15.12);
+INSERT INTO Experiment (Name, Result, Weight) VALUES ('Paulo', 33, 15.12);
+
+DROP TABLE Users;
+
+CREATE TABLE Users (
+	Id INTEGER NOT NULL PRIMARY KEY,
+	Name TEXT,
+	Email TEXT
+);
+
+
+
