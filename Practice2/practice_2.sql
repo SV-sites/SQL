3c70ccfd8fa72b240cd9997348167d6d1b57cc00
SV-sites <sonkauram@gmail.com>
2024-07-16 18:45:07 +0200

    OFFSET

--- Practice2/practice_2.sql
+++ Practice2/practice_2.sql
@@ -4,3 +4,6 @@ SELECT * FROM [New Users] ORDER BY user_id DESC;
 
 INSERT INTO [New Users] (user_id, name) VALUES (11, 'Ferenc');
 SELECT DISTINCT name FROM [New Users];
+
+SELECT * FROM [New Users] LIMIT 5;
+SELECT * FROM [New Users] LIMIT OFFSET 5;
