8ca87c86159a98f9160cad6ecdfeff7c60c9e95c
SV-sites <sonkauram@gmail.com>
2024-07-16 18:33:20 +0200

    new more lines

--- Practice2/practice_2.sql
+++ Practice2/practice_2.sql
@@ -1,3 +1,6 @@
 
 SELECT name, email FROM [New Users];
 SELECT * FROM [New Users] ORDER BY user_id DESC;
+
+INSERT INTO [New Users] (user_id, name) VALUES (11, 'Ferenc');
+SELECT DISTINCT name FROM [New Users];
