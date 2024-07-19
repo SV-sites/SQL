bd784a57afb83d261a355bf8c46cade8d3d3f8ad
SV-sites <sonkauram@gmail.com>
2024-07-19 05:59:20 +0200

    lesson3_2

--- Practice3/practice3.sql
+++ Practice3/practice3.sql
@@ -4,3 +4,8 @@
 SELECT * FROM NewUsers
 WHERE city='Szeged';
 
+-- Limit allways the last one or will be ERROR
+-- ABC by name column and limit 5
+SELECT * FROM NewUsers
+WHERE city='Szeged' 
+ORDER BY name ASC LIMIT 5 ;
